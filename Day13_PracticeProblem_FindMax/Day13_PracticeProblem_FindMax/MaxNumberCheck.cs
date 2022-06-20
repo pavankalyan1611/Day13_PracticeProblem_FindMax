465fd61a2425812a8639e08bd7b821c70c8d7db4
pavankalyan1611 <pkkoppisetti@gmail.com>
2022-06-20 16:25:43 +0530

    [pavan] find max of integer array

---
+++ Day13_PracticeProblem_FindMax/Day13_PracticeProblem_FindMax/MaxNumberCheck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13_FindMax
+{
+    internal class MaxNumberCheck
+    {
+        public static int MaxIntegerNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(firstValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstnumber,secondnumber and thirdnumber are same");
+        }
+    }
+}
---
+++ Day13_PracticeProblem_FindMax/Day13_PracticeProblem_FindMax/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Day13_FindMax
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Maximum value finder");
+            // MaxNumberCheck maxNumberCheck = new MaxNumberCheck();
+            int result = MaxNumberCheck.MaxIntegerNumber(9, 6, 2);
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
