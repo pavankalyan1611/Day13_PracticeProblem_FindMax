d77bf68ad3a6b0ee6a67275780f03e2709c154e4
pavankalyan1611 <105772560+pavankalyan1611@users.noreply.github.com>
2022-07-20 09:49:55 +0530

    Update Program.cs

--- Day13_PracticeProblem_FindMax/Day13_PracticeProblem_FindMax/Program.cs
+++ Day13_PracticeProblem_FindMax/Day13_PracticeProblem_FindMax/Program.cs
@@ -7,10 +7,8 @@ namespace Day13_FindMax
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Maximum value finder");
-            // MaxNumberCheck maxNumberCheck = new MaxNumberCheck();
-            int integers = MaxNumberCheck.MaxIntegerNumber(9, 6, 2);
-            Console.WriteLine("Maximum Of Integer values: {0}",integers);
-            double Double = MaxNumberCheck.MaxFloatNumber(1.2466, 6.6445664664, 5.6456);
+            MaxNumberCheck maxNumberCheck = new MaxNumberCheck(); //Create Object
+            double Double = maxNumberCheck.MaxFloatNumber(1.2466, 6.6445664664, 5.6456);
             Console.WriteLine("Maximum Of floting values: {0}", Double);
 
 
