78533d18c61622b71053c4c4e1e360c3407c9618
pavankalyan1611 <105772560+pavankalyan1611@users.noreply.github.com>
2022-07-20 09:47:30 +0530

    Update MaxNumberCheck.cs

--- Day13_PracticeProblem_FindMax/Day13_PracticeProblem_FindMax/MaxNumberCheck.cs
+++ Day13_PracticeProblem_FindMax/Day13_PracticeProblem_FindMax/MaxNumberCheck.cs
@@ -6,29 +6,10 @@ using System.Threading.Tasks;
 
 namespace Day13_FindMax
 {
-    internal class MaxNumberCheck
+    public class MaxNumberCheck
     {
-        public static int MaxIntegerNumber(int firstValue, int secondValue, int thirdValue)
-        {
-            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
-            {
-                return firstValue;
-            }
-            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
-            {
-                return firstValue;
-            }
-            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
-            {
-                return secondValue;
-            }
-            if (thirdValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(firstValue) > 0)
-            {
-                return thirdValue;
-            }
-            throw new Exception("firstnumber,secondnumber and thirdnumber are same");
-        }
-        public static double MaxFloatNumber(double firstValues, double secondValues, double thirdValues)
+       
+        public double MaxFloatNumber(double firstValues, double secondValues, double thirdValues)
         {
 
             if (firstValues.CompareTo(secondValues) > 0 && firstValues.CompareTo(thirdValues) > 0)
